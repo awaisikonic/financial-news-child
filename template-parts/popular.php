adfa2108a226bdcf1b292ce7d5f09dd19509f30d
dev <awaisikonic@gmail.com>
2025-03-26 00:10:24 +0500

    Minor changes

--- template-parts/popular.php
+++ template-parts/popular.php
@@ -12,11 +12,15 @@ if ($popular_posts->have_posts()) : ?>
         <?php while ($popular_posts->have_posts()) : $popular_posts->the_post(); ?>
             <article>
                 <?php if (has_post_thumbnail()) : ?>
-                    <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title_attribute(); ?>">
+                    <a href="<?php the_permalink(); ?>">
+                        <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title_attribute(); ?>">
+                    </a>
                 <?php else : ?>
-                    <img src="https://via.placeholder.com/300x225" alt="Placeholder Image">
+                    <a href="<?php the_permalink(); ?>">
+                        <img src="https://via.placeholder.com/300x225" alt="Placeholder Image">
+                    </a>
                 <?php endif; ?>
-                <h3><?php the_title(); ?></h3>
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 <p>by <?php the_author(); ?></p>
             </article>
         <?php endwhile; ?>
